1336806f365064722098769a23c5ca5102161cc0
excalibur1702 <aryatathe@gmail.com>
2023-07-23 12:50:19 +0530

    3. minor fixes

---
+++ 5-movie-list-app/README.md
@@ -0,0 +1,3 @@
+# Assignment 5 (Movie List App)
+
+Hosted link: [https://aryatathe.github.io/cogoport-assignments/4-movie-list-app](https://aryatathe.github.io/cogoport-assignments/4-movie-list-app)
---
+++ 5-movie-list-app/script.js
@@ -0,0 +1,41 @@
+var search = document.getElementById("search-input");
+var searchButton = document.getElementById("search-button");
+
+var list = document.getElementById("list");
+
+var movieList = [];
+var movieInfo = {};
+
+const url = (text) => {
+  return `https://www.omdbapi.com/?${text}&apikey=62a2f146`;
+};
+
+const fetchSearch = (text, page) => {
+  fetch(url(`s=${text}&page=${page}`))
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      movieList = data.Search;
+      renderList();
+    });
+};
+
+const movieCard = (movie) => {
+  var movieItem = document.createElement("div");
+  movieItem.className = "movie-item";
+  movieItem.innerHTML = movie.Title;
+  return movieItem;
+};
+
+const renderList = () => {
+  list.innerHTML = "";
+  movieList.forEach((movie) => {
+    list.appendChild(movieCard(movie));
+  });
+};
+
+searchButton.onclick = () => {
+  if (search.value == "") return;
+  fetchSearch(search.value, 1);
+  search.value = "";
+};
